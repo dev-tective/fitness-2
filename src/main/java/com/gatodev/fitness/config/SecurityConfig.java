f0c9951a8fe2734dd835a9c2478b51e962d824d9
Oswaldo Natanael Maguiño <156977615+dev-tective@users.noreply.github.com>
2025-12-15 19:06:55 -0500

    Actualizar SecurityConfig.java

--- src/main/java/com/gatodev/fitness/config/SecurityConfig.java
+++ src/main/java/com/gatodev/fitness/config/SecurityConfig.java
@@ -49,13 +49,12 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/auth/**").permitAll()
-//                        .requestMatchers(HttpMethod.POST, "/user/**").hasAuthority("ADMIN")
-//                        .requestMatchers(HttpMethod.PUT, "/user/**").hasAuthority("ADMIN")
-//                        .requestMatchers(HttpMethod.DELETE, "/user/**").hasAuthority("ADMIN")
-//                        .anyRequest().authenticated()
-//                )
+               .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/user/**").hasAuthority("ADMIN")
+                       .requestMatchers(HttpMethod.PUT, "/user/**").hasAuthority("ADMIN")
+                       .requestMatchers(HttpMethod.DELETE, "/user/**").hasAuthority("ADMIN")
+                      .anyRequest().authenticated()               )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
